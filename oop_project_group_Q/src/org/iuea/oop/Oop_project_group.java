d6b6f4dda26e606e501ac3dfd0cbdbdd3db76a26
bamata <subamamaurice@gmail.com>
2019-09-27 20:19:02 +0300

    added group info under the packages

--- oop_project_group_Q/src/org/iuea/oop/Oop_project_group.java
+++ oop_project_group_Q/src/org/iuea/oop/Oop_project_group.java
@@ -1,9 +1,18 @@
 
 package org.iuea.oop;
-
-import org.iuea.oop.model.Courses;
-import org.iuea.oop.model.Student;
 import org.iuea.oop.view.LoginView;
+
+/*
+        Group members
+        =============
+        
+        Name                                 Registration Number
+        ====                                 ===================
+        BAMATA WA BAMATA Maurice             18/420/BSSE-S
+        NDAHURA BASARA Moses                 19/231/BIT-S
+        ALLY KALIMA                          18/1097/BIT-S
+        NANNYUNJA NOELINA                    19/536/BSCS-S
+        */
 
 public class Oop_project_group {
 
@@ -15,14 +24,4 @@ public class Oop_project_group {
     }
    
 }
-        /*
-        Group members
-        =============
         
-        Name                                 Registration Number
-        ====                                 ===================
-        BAMATA WA BAMATA Maurice             18/420/BSSE-S
-        NDAHURA BASARA Moses                 19/231/BIT-S
-        ALLY KALIMA                          18/1097/BIT-S
-        NANNYUNJA NOELINA                    19/536/BSCS-S
-        */
