706f314818e013d0e6640a7fc446139225f821b4
bamata <subamamaurice@gmail.com>
2019-10-02 12:49:14 +0300

    initial commit

---
+++ src/org/iuea/oop/Main.java
@@ -0,0 +1,27 @@
+
+package org.iuea.oop;
+import org.iuea.oop.view.LoginView;
+
+/*
+        Group members
+        =============
+        
+        Name                                 Registration Number
+        ====                                 ===================
+        BAMATA WA BAMATA Maurice             18/420/BSSE-S
+        NDAHURA BASARA Moses                 19/231/BIT-S
+        ALLY KALIMA                          18/1097/BIT-S
+        NANNYUNJA NOELINA                    19/536/BSCS-S
+        */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       LoginView log = new LoginView(); // login window 
+
+       
+    }
+   
+}
+        
---
+++ src/org/iuea/oop/modall/CourseUnits.java
@@ -0,0 +1,32 @@
+
+package org.iuea.oop.modall;
+
+
+/*
+        Group members
+        =============
+        Name                                 Registration Number
+        ====                                 ===================
+        BAMATA WA BAMATA Maurice             18/420/BSSE-S
+        NDAHURA BASARA Moses                 19/231/BIT-S
+        ALLY KALIMA                          18/1097/BIT-S
+        NANNYUNJA NOELINA                    19/536/BSCS-S
+        */
+
+public class CourseUnits {
+    
+    String name;
+    int credit_unit;
+    String description;
+    String content;
+    String course_code;
+    String year;
+    
+    public CourseUnits(){}
+
+    
+    
+}
+
+
+        
