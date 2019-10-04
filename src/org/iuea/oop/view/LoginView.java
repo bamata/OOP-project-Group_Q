3b7c70e2843406a950c02417808d8322979c1b19
bamata <47469850+bamata@users.noreply.github.com>
2019-10-04 15:47:09 +0300

    Update LoginView.java

--- src/org/iuea/oop/view/LoginView.java
+++ src/org/iuea/oop/view/LoginView.java
@@ -1,3 +1,15 @@
+/*
+        Group members
+        =============
+        Name                                 Registration Number
+        ====                                 ===================
+        BAMATA WA BAMATA Maurice             18/420/BSSE-S
+        NDAHURA BASARA Moses                 19/231/BIT-S
+        ALLY KALIMA                          18/1097/BIT-S
+        NANNYUNJA NOELINA                    19/536/BSCS-S
+        HUSSAIN AKIL BHARMAL                 18/563/BIT-S
+        */
+
 
 package org.iuea.oop.view;
 
@@ -17,16 +29,7 @@ import javax.swing.JTextField;
 import javax.swing.JWindow;
 
 
- /*
-        Group members
-        =============
-        Name                                 Registration Number
-        ====                                 ===================
-        BAMATA WA BAMATA Maurice             18/420/BSSE-S
-        NDAHURA BASARA Moses                 19/231/BIT-S
-        ALLY KALIMA                          18/1097/BIT-S
-        NANNYUNJA NOELINA                    19/536/BSCS-S
-        */
+ 
 
 public class LoginView  extends JFrame{
    //objects
